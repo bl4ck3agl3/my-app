a8b2337a63e2a7aa1539165d0c080cb8156d11a8
bl4ck3agl3 <37936044+bl4ck3agl3@users.noreply.github.com>
2023-06-14 05:19:30 +0200

    Update about.js

--- components/about.js
+++ components/about.js
@@ -80,7 +80,7 @@ export default function About() {
                     </p>
                     <div className="mt-10 flex items-center gap-x-6">
                         <a
-                            href="#"
+                            href="/register"
                             className="rounded-md bg-gray-950 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-950"
                         >
                             Register
